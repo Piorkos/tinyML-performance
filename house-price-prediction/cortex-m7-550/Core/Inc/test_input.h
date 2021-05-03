3c3de54a997d4b08788a42dd42ee54f61162ce47
Dominik Piórkowski <dominikpiorkowski@Dominiks-MacBook-Pro.local>
2021-05-03 17:10:49 +0200

    Working ML inference for house price prediction.

---
+++ house-price-prediction/cortex-m7-550/Core/Inc/test_input.h
@@ -0,0 +1,19 @@
+/*
+ * test_input.h
+ *
+ *  Created on: 3 May 2021
+ *      Author: dominikpiorkowski
+ */
+
+#ifndef SRC_TEST_INPUT_H_
+#define SRC_TEST_INPUT_H_
+
+
+#include "network.h"
+
+extern float aiInDataHouse0[AI_NETWORK_IN_1_SIZE];
+extern float aiInDataHouse1[AI_NETWORK_IN_1_SIZE];
+extern float aiInDataHouse2[AI_NETWORK_IN_1_SIZE];
+
+
+#endif /* SRC_TEST_INPUT_H_ */
