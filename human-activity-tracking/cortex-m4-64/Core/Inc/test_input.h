503a5160dbd01118123c1404bdb884db069e7c37
Dominik Piórkowski <dominikpiorkowski@Dominiks-MacBook-Pro.local>
2021-05-04 07:27:00 +0200

    Working ML inference for human activity tracking.

--- human-activity-tracking/cortex-m4-64/Core/Inc/test_input.h
+++ human-activity-tracking/cortex-m4-64/Core/Inc/test_input.h
@@ -9,7 +9,11 @@
 #define INC_TEST_INPUT_H_
 
 
+#include "network.h"
 
+extern float aiInDataStationary[AI_NETWORK_IN_1_SIZE];
+extern float aiInDataWalking[AI_NETWORK_IN_1_SIZE];
+extern float aiInDataRunning[AI_NETWORK_IN_1_SIZE];
 
 
 #endif /* INC_TEST_INPUT_H_ */
--- human-activity-tracking/cortex-m4-64/Core/Src/main.c
+++ human-activity-tracking/cortex-m4-64/Core/Src/main.c
@@ -6,8 +6,10 @@
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
+  * Copyright (c)	2021 STMicroelectronics,
+  * 				2021 Dominik Piórkowski.
+  *
+  * All rights reserved.
   *
   * This software component is licensed by ST under BSD 3-Clause license,
   * the "License"; You may not use this file except in compliance with the
@@ -26,7 +28,12 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <stdio.h>
 
+#include "ai_platform.h"
+#include "network.h"
+#include "network_data.h"
+#include "test_input.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -46,13 +53,26 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+char buf[50];
+int buf_len = 0;
+ai_handle network;
 
+uint32_t timer_counter = 0;
+uint32_t timestamp = 0;
+
+float aiOutData[3][AI_NETWORK_OUT_1_SIZE];
+uint8_t activations[AI_NETWORK_DATA_ACTIVATIONS_SIZE];
+const char* labels[AI_NETWORK_OUT_1_SIZE] = {
+		"stationary", "walking", "running"
+};
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-
+static void AI_Init(ai_handle w_addr, ai_handle act_addr);
+static void AI_Run(float *pIn, float *pOut);
+static uint32_t argmax(const float * values, uint32_t len);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -67,7 +87,13 @@ void SystemClock_Config(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+	//  normalise data to range [-1,1]
+	for(int i = 0; i < AI_NETWORK_IN_1_SIZE; i++)
+	{
+		aiInDataStationary[i] = aiInDataStationary[i] / 4000.0;
+		aiInDataWalking[i] = aiInDataWalking[i] / 4000.0;
+		aiInDataRunning[i] = aiInDataRunning[i] / 4000.0;
+	}
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -91,14 +117,46 @@ int main(void)
   MX_CRC_Init();
   MX_TIM16_Init();
   MX_USART1_UART_Init();
-  /* USER CODE BEGIN 2 */
 
+  /* USER CODE BEGIN 2 */
+  AI_Init(ai_network_data_weights_get(), activations);
+  //  Start timer/counter
+  HAL_TIM_Base_Start_IT(&htim16);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  buf_len = sprintf(buf, "M4-64 Running HAT inference \n");
+	  HAL_UART_Transmit(&huart1, (uint8_t *)buf, buf_len, 100);
+
+	  timestamp = htim16.Instance->CNT;
+
+	  //	  stationary
+	  AI_Run(aiInDataStationary, aiOutData[0]);
+
+	  //	  walking
+	  AI_Run(aiInDataWalking, aiOutData[1]);
+
+	  //	  running
+	  AI_Run(aiInDataRunning, aiOutData[2]);
+
+
+	  uint32_t predicted_class_1 = argmax(aiOutData[0], AI_NETWORK_OUT_1_SIZE);
+	  uint32_t predicted_class_2 = argmax(aiOutData[1], AI_NETWORK_OUT_1_SIZE);
+	  uint32_t predicted_class_3 = argmax(aiOutData[2], AI_NETWORK_OUT_1_SIZE);
+
+
+	  buf_len = sprintf(buf, "Inference duration: (%lu s) + %lu*0.1ms \n", timer_counter, (htim16.Instance->CNT - timestamp));
+	  HAL_UART_Transmit(&huart1, (uint8_t *)buf, buf_len, 100);
+
+	  buf_len = sprintf(buf, "1 predicted class: %d - %s \n", (int) predicted_class_1, labels[predicted_class_1]);
+	  HAL_UART_Transmit(&huart1, (uint8_t *)buf, buf_len, 100);
+	  buf_len = sprintf(buf, "2 predicted class: %d - %s \n", (int) predicted_class_2, labels[predicted_class_2]);
+	  HAL_UART_Transmit(&huart1, (uint8_t *)buf, buf_len, 100);
+	  buf_len = sprintf(buf, "3 predicted class: %d - %s \n", (int) predicted_class_3, labels[predicted_class_3]);
+	  HAL_UART_Transmit(&huart1, (uint8_t *)buf, buf_len, 100);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -171,7 +229,77 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	if (htim == &htim16 )
+	{
+		++timer_counter;
+	}
+}
 
+
+static void AI_Init(ai_handle w_addr, ai_handle act_addr)
+{
+	ai_error err;
+
+	/* 1 - Create an instance of the model */
+	err = ai_network_create(&network, AI_NETWORK_DATA_CONFIG);
+	if (err.type != AI_ERROR_NONE) {
+		printf("ai_network_create error - type=%d code=%d\r\n", err.type, err.code);
+		Error_Handler();
+	}
+
+	/* 2 - Initialize the instance */
+	const ai_network_params params = {
+			AI_NETWORK_DATA_WEIGHTS(w_addr),
+			AI_NETWORK_DATA_ACTIVATIONS(act_addr)
+	};
+
+	if (!ai_network_init(network, &params))
+	{
+		err = ai_network_get_error(network);
+		printf("ai_network_init error - type=%d code=%d\r\n", err.type, err.code);
+		Error_Handler();
+	}
+}
+static void AI_Run(float *pIn, float *pOut)
+{
+	ai_i32 batch;
+	ai_error err;
+
+	/* 1 - Create the AI buffer IO handlers with the default definition */
+	ai_buffer ai_input[AI_NETWORK_IN_NUM] = AI_NETWORK_IN;
+	ai_buffer ai_output[AI_NETWORK_OUT_NUM] = AI_NETWORK_OUT;
+
+	/* 2 - Update IO handlers with the data payload */
+	ai_input[0].n_batches = 1;
+	ai_input[0].data = AI_HANDLE_PTR(pIn);
+	ai_output[0].n_batches = 1;
+	ai_output[0].data = AI_HANDLE_PTR(pOut);
+
+	batch = ai_network_run(network, ai_input, ai_output);
+	if (batch != 1)
+	{
+		err = ai_network_get_error(network);
+		buf_len = sprintf(buf, "AI ai_network_run error - type=%d code=%d \n", err.type, err.code);
+		HAL_UART_Transmit(&huart1, (uint8_t *)buf, buf_len, 100);
+		Error_Handler();
+	}
+}
+static uint32_t argmax(const float * values, uint32_t len)
+{
+	float max_value = values[0];
+	uint32_t max_index = 0;
+	for (uint32_t i = 1; i < len; i++)
+	{
+		if (values[i] > max_value)
+		{
+			max_value = values[i];
+			max_index = i;
+		}
+	}
+	return max_index;
+}
 /* USER CODE END 4 */
 
 /**
