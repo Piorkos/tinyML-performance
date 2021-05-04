eae1abdb61e6f87989af1e1145380bcf66cb954c
Dominik Piórkowski <dominikpiorkowski@Dominiks-MacBook-Pro.local>
2021-05-04 07:15:50 +0200

    Project created.

---
+++ human-activity-tracking/cortex-m4-64/Core/Inc/test_input.h
@@ -0,0 +1,15 @@
+/*
+ * test_input.h
+ *
+ *  Created on: May 4, 2021
+ *      Author: dominikpiorkowski
+ */
+
+#ifndef INC_TEST_INPUT_H_
+#define INC_TEST_INPUT_H_
+
+
+
+
+
+#endif /* INC_TEST_INPUT_H_ */
---
+++ human-activity-tracking/cortex-m4-64/Core/Src/test_input.cpp
@@ -0,0 +1,10 @@
+/*
+ * test_input.cpp
+ *
+ *  Created on: May 4, 2021
+ *      Author: dominikpiorkowski
+ */
+
+
+
+
